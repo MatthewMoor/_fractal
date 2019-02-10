24d5a6174084f50eb81394752a2d14530ff70c12
Matthew Moor <meccep.98.98@mail.ru>
2019-02-10 14:18:55 +0300

    fold

--- triangle_Serp.py
+++ triangle_Serp.py
@@ -1,6 +1,7 @@
 import turtle as tr
 
 def triangle(points, myTurtle):
+    # draws a triangle
     myTurtle.up()
     myTurtle.goto(points[0][0], points[0][1])
     myTurtle.down()
@@ -10,12 +11,15 @@ def triangle(points, myTurtle):
 
 
 def getMiddle(p1, p2):
+    # find the middle
     return ( (p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
 
 
 def sierpinski(points, degree, myTurtle):
+    # degree of triangle
     triangle(points, myTurtle)
     if degree > 0:
+
         sierpinski([points[0], getMiddle(points[0], points[1]), getMiddle(points[0], points[2])], degree-1, myTurtle)
         sierpinski([points[1], getMiddle(points[0], points[1]), getMiddle(points[1], points[2])], degree-1, myTurtle)
         sierpinski([points[2], getMiddle(points[2], points[1]), getMiddle(points[0], points[2])], degree-1, myTurtle)
@@ -25,9 +29,8 @@ def main():
     myTurtle = tr.Turtle()
     myWin = tr.Screen()
     points = [[0, 0], [-230, -300], [230, -300]]
-    degree = 4
+    # deeper
+    degree = 5
     sierpinski(points, degree, myTurtle)
     myWin.exitonclick()
-    ts = tr.getscreen()
-    ts.getcanvas().postscript(file="fractal.eps")
 main()
